6c47a1aec0370c6aa29d7c1ef71e2e1a002c0463
trlgml <1229771221@qq.com>
2021-03-08 09:37:35 +0800

    feat:test db

--- src/models/mongoose/demo.js
+++ src/models/mongoose/demo.js
@@ -2,7 +2,32 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var SchemaInstance = new Schema({
-  id: { type: String, default: '' },
+  // id: { type: String, default: '' },
+  // type: { type: String, default: '' },
+  // name: {
+  //   first: String,
+  //   last: String
+  // },
+  // tags: { type: [String], index: true } // field level
+
+  "name": String,
+  "sex": String,
+  "age": Number,
+  "create_time": Date
 });
 
-module.exports = mongoose.model('demo', SchemaInstance);
+// 实例方法
+SchemaInstance.methods.customMethods = function (cb) {
+  return this.model('Demo').find({ type: this.type }, cb);
+};
+
+// 静态方法
+SchemaInstance.statics.customStatics = function (type, cb) {
+  return this.find({ type: new RegExp(type, 'i') }, cb)
+};
+
+SchemaInstance.virtual('fullName').get(function () {
+  return this.name.first + ' ' + this.name.last;
+});
+
+module.exports = mongoose.model('Demo', SchemaInstance);
--- src/services/mongooseServices.js
+++ src/services/mongooseServices.js
@@ -1,17 +1,51 @@
 
 const model = require('../connection/mongoose');
-const { demo } = model
+const { Demo } = model
 
 const DemoList = async () => {
-  const story = await demo.find().exec()
+  // // 查询每个 last name 是 'Ghost' 的 person， select `name` 和 `occupation` 字段
+  // Person.findOne({ 'name.last': 'Ghost' }, 'name occupation', function (err, person) {
+  //   if (err) return handleError(err);
+  //   // Prints "Space Ghost is a talk show host".
+  //   console.log('%s %s is a %s.', person.name.first, person.name.last,
+  //     person.occupation);
+  // });
+  const story = await Demo.find().exec()
   return story
 }
 
 const createDemo = async () => {
-  const story = await demo({ id: 100 }).save()
+  const story = await new Demo({ id: 100 }).save()
+  // await Demo.create({ id: 200 })
+
   return story
 }
 
+// Demo.create({
+//   "name": "23",
+//   "sex": "女",
+//   "age": 22,
+//   "create_time": "2019-01-11 15:09:59"
+// })
+// Demo.aggregate([
+//   {
+//     $project: {
+//       "year": { "$substr": ["$create_time", 0, 4] },
+//       "month": { "$substr": ["$create_time", 5, 2] },
+//       "day": { "$substr": ["$create_time", 8, 2] }
+//     }
+//   },
+//   {
+//     $group: {
+//       _id: { year: "$year", month: "$month", day: "$day" },
+//       total: { $sum: 1 }
+//     }
+//   }
+// ]).then((result) => {
+//   console.log(result)
+// }).catch((err) => {
+
+// });
 module.exports = {
   DemoList,
   createDemo
